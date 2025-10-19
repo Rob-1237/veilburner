40edee7cb00f2d39d03c06e75df382e08843d093
Rob-1237 <rchambers1237@gmail.com>
2025-10-19 11:58:56 -0400

    Updated Reviews and Interviews, deleted News.

--- src/data/interviews-written.js
+++ src/data/interviews-written.js
@@ -3,24 +3,40 @@ export const interviewsWritten = [
     quote: "Extreme metal duo Veilburner take off their masks to discuss their seventh album The Duality of Decapitation and Wisdom.",
     fullText: "In this episode, Mephisto Deleterio and Chrisom Infernium delve into the creation of their latest album, sharing insights into their songwriting process, thematic explorations, and the evolution of their sound.",
     publication: "Tom Osman Substack",
-    date: "October 2024",
+    date: "November 5, 2024",
+    author: "Tom Osman",
     link: "https://tomosman.substack.com/p/episode-42-veilburner"
   },
   {
-    quote: "Mephisto Deleterio and Chrisom Infernium of Veilburner join me this week for their first ever long-form podcast interview.",
-    fullText: "We discuss their upcoming record, why they don't play live, their approach to songwriting, Mephisto's collection of oddities and more.",
-    publication: "Into the Necrosphere",
-    date: "September 2023",
-    link: "https://intothenecrosphere.podbean.com/e/156-veilburner-s-mind-bending-black-metal-masterclass-decibel-top-40-dissected-review-round-up-more/"
+    quote: "The Extreme Metal duo of multi-instrumentalist Mephisto Deleterio and vocalist Chrisom Infernium always have plenty of insight into their fascinating, musical explorations.",
+    fullText: "I really feel like I “found myself” when we had finished writing it.",
+    publication: "Blessed Altar Zine",
+    date: "December 1, 2022",
+    author: "Tom Osman",
+    link: "https://blessedaltarzine.com/2022/12/01/veilburner-interview-3/"
   },
   {
-    quote: "Veilburner are a tremendously creative Extreme Metal duo from Pennsylvania, USA.",
-    fullText: "Right at the end of 2018 they released the album The Penance, which was a significant step forward in their sound. The album was well-received and showcased their unique blend of black and death metal elements.",
+    quote: "We’re trying to stimulate as many of the five senses as we can.",
+    fullText: "Mostly the lyrics follow along with the story from A Sire, but ...it plays heavily on psychology with our usual occult atmosphere.",
+    publication: "Mosh Pit Nation",
+    date: "October 11, 2021",
+    author: " Hayduke X",
+    link: "https://www.moshpitnation.com/interview-veilburner/"
+  },
+  {
+    quote: "With the excellent follow-up record Lurkers in the Capsule of Skull due out on September 24th I caught up with the band again to hear about the new record and find out how they’ve been doing in these crazy times",
+    fullText: "The circumstances of the pandemic and lockdown created a good opportunity to be creative. For people fortunate enough to have a creative outlet, it was the perfect opportunity to lose yourself in it. ",
     publication: "Blessed Altar Zine",
     date: "September 6, 2021",
-    link: "https://blessedaltarzine.com/tag/veilburner/"
-  }
+    author: "Tom Boatman",
+    link: "https://blessedaltarzine.com/2021/09/06/veilburner-interview-2/"
+  },
+  {
+    quote: "The Philadelphia two-piece’s debut album The Three Lightbearers seems to have gone largely under the radar, but this is a band whose voice need be heard.",
+    fullText: "The challenge is being open to ideas and incorporating a variety of musical influences while still retaining a cohesive identity. I don’t want to write albums that are all over the map without something central to keep everything in a common orbit.",
+    publication: "Toilet ov Hell",
+    date: "October 17, 2014",
+    author: "Christian Molenaar",
+    link: "https://toiletovhell.com/veilburner-the-toilet-ov-hell-interview/"
+  },
 ];
-
-
-// https://toiletovhell.com/veilburner-the-toilet-ov-hell-interview/
